971ec315e51991e2a5b3f2db586e31a2df939cbb
Neri Laredo <nerila1310@gmail.com>
2021-08-02 23:44:59 -0500

    CSS al resumen

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,4 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span{
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+
+    span{
+        font-weight: bold;
+    }
+`;
 
 const Cotizacion = ({resultado}) => {
     
@@ -6,13 +28,13 @@ const Cotizacion = ({resultado}) => {
     console.log(resultado);
     
     return (  
-        <div>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>El precio más alto del dia: <span>{resultado.HIGHDAY}</span></p>
-            <p>El precio más bajo del día: <span>{resultado.LOWDAY}</span></p>
-            <p>Variación ultimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></p>
-            <p>Última actualización: <span>{resultado.LASTUPDATE}</span></p>
-        </div>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>El precio más alto del dia: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>El precio más bajo del día: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variación ultimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></Info>
+            <Info>Última actualización: <span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
     );
 }
  
